420364ef6278cc1dc8f8a6a3f68c9aabd893e61f
dillanj <dillan.johnson1@dmail.dixie.edu>
2021-09-07 17:51:27 -0600

    section 34 done

--- Section 3/Operators/src/com/company/Main.java
+++ Section 3/Operators/src/com/company/Main.java
@@ -3,7 +3,43 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int result = 1 + 3;
+	    int result = 1 + 2;
         System.out.println("1 + 2 = " +  result);
+
+        int previousResult = result;
+        System.out.println("Previous Result " + previousResult);
+        result = result - 1;
+        System.out.println(" 3 - 1 = " + result);
+        System.out.println("Previous Result = " +  previousResult);
+
+        result = result * 10; // 2 * 10 = 20
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;  // 20 / 5 = 4
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3; // the remainder of 4 / 3 (1)
+        System.out.println("4 % 3 = " + result);
+
+        result++; // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        result--; // 2 - 1 = 1
+        System.out.println("2 - 1 " + result);
+
+        result += 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        boolean isAlien = false;
+        if (isAlien){
+            System.out.println("It is an alien!");
+        }
+        else {
+            System.out.println("It is not an Alien!");
+        }
+
     }
 }
