75a73563a484936b67cae2ba9047e3b20e26aad3
dillanj <dillan.johnson1@dmail.dixie.edu>
2021-09-09 12:38:05 -0600

    operator challenge complete

--- Section 3/Operators/src/com/company/Main.java
+++ Section 3/Operators/src/com/company/Main.java
@@ -3,48 +3,72 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int result = 1 + 2;
-        System.out.println("1 + 2 = " +  result);
+//	    int result = 1 + 2;
+//        System.out.println("1 + 2 = " +  result);
+//
+//        int previousResult = result;
+//        System.out.println("Previous Result " + previousResult);
+//        result = result - 1;
+//        System.out.println(" 3 - 1 = " + result);
+//        System.out.println("Previous Result = " +  previousResult);
+//
+//        result = result * 10; // 2 * 10 = 20
+//        System.out.println("2 * 10 = " + result);
+//
+//        result = result / 5;  // 20 / 5 = 4
+//        System.out.println("20 / 5 = " + result);
+//
+//        result = result % 3; // the remainder of 4 / 3 (1)
+//        System.out.println("4 % 3 = " + result);
+//
+//        result++; // 1 + 1 = 2
+//        System.out.println("1 + 1 = " + result);
+//
+//        result--; // 2 - 1 = 1
+//        System.out.println("2 - 1 " + result);
+//
+//        result += 2; // 1 + 2 = 3
+//        System.out.println("1 + 2 = " + result);
+//
+//        result *= 10; // 3 * 10 = 30
+//        System.out.println("3 * 10 = " + result);
+//
+//        boolean isAlien = false;
+//        if (isAlien){
+//            System.out.println("It is an alien!");
+//        }
+//        else {
+//            System.out.println("It is not an Alien!");
+//        }
+//
+//        if(isAlien)
+//            System.out.println("It is not an Alien (without code block)");
+//        else
+//            System.out.println("It not an alient (without code block)");
+//
+//        int topScore = 100;
+//        if(topScore > 100){
+//            System.out.println("You got the high score!");
+//        }
+//
+//        int secondTopScore = 60;
+//        if(topScore > secondTopScore && topScore <= 100){
+//            System.out.println("Greater than top score and less than or equal to 100");
+//        }
 
-        int previousResult = result;
-        System.out.println("Previous Result " + previousResult);
-        result = result - 1;
-        System.out.println(" 3 - 1 = " + result);
-        System.out.println("Previous Result = " +  previousResult);
-
-        result = result * 10; // 2 * 10 = 20
-        System.out.println("2 * 10 = " + result);
-
-        result = result / 5;  // 20 / 5 = 4
-        System.out.println("20 / 5 = " + result);
-
-        result = result % 3; // the remainder of 4 / 3 (1)
-        System.out.println("4 % 3 = " + result);
-
-        result++; // 1 + 1 = 2
-        System.out.println("1 + 1 = " + result);
-
-        result--; // 2 - 1 = 1
-        System.out.println("2 - 1 " + result);
-
-        result += 2; // 1 + 2 = 3
-        System.out.println("1 + 2 = " + result);
-
-        result *= 10; // 3 * 10 = 30
-        System.out.println("3 * 10 = " + result);
-
-        boolean isAlien = false;
-        if (isAlien){
-            System.out.println("It is an alien!");
+        /// OPERATOR CHALLENGE
+        double a = 20.00; // 1
+        double b = 80.00; // 2
+        double result = (a + b) * 100.00; // 3
+        double remainder = result % 40.00; // 4
+        boolean noRemainder = remainder == 0 ? true : false; // 5
+        System.out.println("has Remainder: " + noRemainder); // 6
+        if(!noRemainder){ // 7
+            System.out.println("Got some remainder");
         }
-        else {
-            System.out.println("It is not an Alien!");
-        }
 
-        if(isAlien)
-            System.out.println("It is not an Alien (without code block)");
-        else
-            System.out.println("It not an alient (without code block)");
+
+
 
     }
 }
