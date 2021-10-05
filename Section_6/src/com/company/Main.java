d8f18ca2e1171b6be3650a265a69a42aee26cfc4
dillanj <dillan.johnson1@dmail.dixie.edu>
2021-10-04 22:49:26 -0600

    section 6 started. 77 & 78 done

---
+++ Section_6/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // LESSON 77 & 78
+        Car porshe = new Car();
+        Car kia = new Car();
+        porshe.setModel("Carrera");
+        System.out.println("The model for our porsche is: " + porshe.getModel());
+    }
+}
