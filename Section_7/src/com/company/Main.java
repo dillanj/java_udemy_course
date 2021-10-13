45070b7ca987e057ab6e07e8dd05790fcb2b0678
dillanj <dillan.johnson1@dmail.dixie.edu>
2021-10-12 22:53:32 -0600

    lesson 96 - polymorphism done

--- Section_7/src/com/company/Main.java
+++ Section_7/src/com/company/Main.java
@@ -1,15 +1,114 @@
 package com.company;
+// LESSON 96 CHALLENGE
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name){
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine(){
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate(){
+        return "Car -> accelerate()";
+    }
+
+    public String brake(){
+        return "Car -> brake()";
+    }
+}
+
+class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Mitsubishi -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Mitsubishi -> brake()";
+    }
+}
+
+class Tesla extends Car {
+    public Tesla(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
+
+
 
 public class Main {
 
     public static void main(String[] args) {
         // LESSON 94 CHALLENGE
-        Printer printer = new Printer(50, false);
-        System.out.println("Initial Page Count = " + printer.getPagesPrinted());
-        int pagesPrinted = printer.printPages(4);
-        System.out.println("Pages Printed Was:  " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
-        pagesPrinted = printer.printPages(2);
-        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+//        Printer printer = new Printer(50, false);
+//        System.out.println("Initial Page Count = " + printer.getPagesPrinted());
+//        int pagesPrinted = printer.printPages(4);
+//        System.out.println("Pages Printed Was:  " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+//        pagesPrinted = printer.printPages(2);
+//        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = " + printer.getPagesPrinted());
+
+        // LESSON 96 CHALLENGE
+        Car car = new Car(8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Tesla mitsubishi = new Tesla(6, "Outlander VRW 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Ford ford = new Ford(6, "F150");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+
+        Tesla tesla = new Tesla(6, "Model S");
+        System.out.println(tesla.startEngine());
+        System.out.println(tesla.accelerate());
+        System.out.println(tesla.brake());
 
     }
 }
