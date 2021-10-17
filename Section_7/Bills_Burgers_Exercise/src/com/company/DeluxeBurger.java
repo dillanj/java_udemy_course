cb7db8c5c8d9b8deef93b3dae0bd34c4a66bea52
dillanj <dillan.johnson1@dmail.dixie.edu>
2021-10-16 20:58:19 -0600

    lesson 98 finished

---
+++ Section_7/Bills_Burgers_Exercise/src/com/company/DeluxeBurger.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class DeluxeBurger extends Hamburger {
+    public DeluxeBurger() {
+        super("Deluxe Burger", "Beef & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips", 2.75);
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot add extras to the deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot add extras to the deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot add extras to the deluxe burger.");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot add extras to the deluxe burger.");
+    }
+}
---
+++ Section_7/Bills_Burgers_Exercise/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Hamburger hamburger= new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+        System.out.println("Total burger price: " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHamburgerAddition2("Lentils", 3.41);
+        System.out.println("Total healthy burger price is : " + healthyBurger.itemizeHamburger());
+
+        DeluxeBurger db = new DeluxeBurger();
+        db.addHamburgerAddition3("Should not do this", 0);
+        db.itemizeHamburger();
+
+    }
+}
