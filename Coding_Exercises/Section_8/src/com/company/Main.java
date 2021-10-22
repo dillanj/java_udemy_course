af64bfbda0df737ebd1582974ceaf792ea5465db
dillanj <dillan.johnson1@dmail.dixie.edu>
2021-10-22 13:43:07 -0600

    lesson 108 finished

--- Coding_Exercises/Section_8/src/com/company/Main.java
+++ Coding_Exercises/Section_8/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // CODING CHALLENGE 41
-        SortedArray.runProgram();
+//        SortedArray.runProgram();
         // CODING CHALLENGE 42
 //	    MinimumElement.runProgram();
         // CODING CHALLENGE 43
