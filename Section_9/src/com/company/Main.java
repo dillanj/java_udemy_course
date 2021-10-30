72a7cd8b01fdefb5adfe168728ae7dadb3852a82
dillanj <dillan.johnson1@dmail.dixie.edu>
2021-10-29 22:24:12 -0600

    section 9 started

---
+++ Section_9/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ITelephone dillansPhone;
+        dillansPhone = new DeskPhone(12345);
+        dillansPhone.powerOn();
+        dillansPhone.callPhone(12345);
+        dillansPhone.answer();
+    }
+}
